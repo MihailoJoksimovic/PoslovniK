8a4cd67f5c3a2d2cd2e45e0106219f9999106c83
Mihailo Joksimovic <tinzey@gmail.com>
2016-06-19 22:32:57 +0200

    Editovanje osoba - Part 1

--- src/main/webapp/js/app/View/AdminDashboard.js
+++ src/main/webapp/js/app/View/AdminDashboard.js
@@ -4,7 +4,8 @@ Poslovnik.AdminDashboard = Backbone.View.extend({
     events: {
         'click #add-new-btn' : 'onAddNewBtnClick',
         'click .delete-row' : 'onDeleteRowBtnClick',
-        'click .save-row' : 'onSaveRowBtnClick'
+        'click .save-row' : 'onSaveRowBtnClick',
+        'click .edit-row' : 'onEditRowBtnClick'
     },
     
     initialize: function() {
@@ -55,18 +56,54 @@ Poslovnik.AdminDashboard = Backbone.View.extend({
         });
     },
     
+    getPersonEditRowHtml: function(person) {
+        var row = "";
+        
+        row+="<td style='text-align: center;'></td>";
+        row+='<td><input type="email" name="email" value="<%= email %>" /></td>';
+        row+='<td><input type="password" name="password" value="<%= password %>" /></td>';
+        row+='<td><select name="title"><option value="mr" <%= (title == "mr") ? "selected" : ""  %>>mr</option><option value="ms" <%= (title == "ms") ? "selected" : ""  %>>ms</option></select></td>';
+        row+="<td><input type='text' name='first_name' value='<%= first_name %>' /></td>";
+        row+="<td><input type='text' name='last_name' value='<%= last_name %>' /></td>";
+        row+="<td><%= position_html %></td>";
+        row+="<td><%= account_type_html %></td>";
+        
+        var positionHtml = '<select name="position">';
+        
+        Poslovnik.Positions.each(function(position) {
+            var selected = person.get('position_name') == position.get('name') ? 'selected' : '';
+            
+            positionHtml += '<option value="'+position.get('id')+'" '+selected+'>'+position.get('name')+'</option>';
+        });
+        positionHtml += "</select>";
+        
+        var accountTypeHtml = '<select name="account_type">';
+        accountTypeHtml += '<option value="'+Poslovnik.PermissionLevels.ADMINISTRATOR+'" '+(person.get('permission_level') == Poslovnik.PermissionLevels.ADMINISTRATOR ? 'selected' : '')+'>Administrator</option>';
+        accountTypeHtml += '<option value="'+Poslovnik.PermissionLevels.MODERATOR+'" '+(person.get('permission_level') == Poslovnik.PermissionLevels.MODERATOR ? 'selected' : '')+'>Moderator</option>';
+        accountTypeHtml += '<option value="'+Poslovnik.PermissionLevels.REGULAR_USER+'" '+(person.get('permission_level') == Poslovnik.PermissionLevels.REGULAR_USER ? 'selected' : '')+'>Employee</option>';
+        accountTypeHtml += "</select>";
+        
+        var template = _.template(row);
+        
+        var rowHtml = template({
+            cid: person.cid,
+            email: person.get("email"),
+            password: person.get("password"),
+            first_name: person.get('first_name'),
+            last_name: person.get('last_name'),
+            title: person.get("title"),
+            position_html: positionHtml,
+            account_type_html: accountTypeHtml
+        });
+        
+        return rowHtml;
+    },
+    
     getNewPersonRowHtml: function(person) {
         var id = person.get('id');
         
         var row = "<tr data-isnew='1' data-cid='<%= cid %>'>";
-        row+="<td style='text-align: center;'></td>";
-        row+='<td><input type="email" name="email" /></td>';
-        row+='<td><input type="password" name="password" /></td>';
-        row+='<td><select name="title"><option value="mr">mr</option><option value="ms">ms</option></select></td>';
-        row+="<td><input type='text' name='first_name' /></td>";
-        row+="<td><input type='text' name='last_name' /></td>";
-        row+="<td><%= position_html %></td>";
-        row+="<td><%= account_type_html %></td>";
+        row += this.getPersonEditRowHtml(person);
         row += "<td>";
         row += "<a href='javascript: void(0);' style='font-size: 16px' data-cid='<%= cid %>' class='save-row glyphicon glyphicon-floppy-disk'></a>&nbsp;";
         row += "<a href='javascript: void(0);' style='font-size: 16px' data-cid='<%= cid %>' class='delete-row glyphicon glyphicon-remove'></a>";
@@ -75,23 +112,8 @@ Poslovnik.AdminDashboard = Backbone.View.extend({
         
         var template = _.template(row);
         
-        var positionHtml = '<select name="position">';
-        
-        Poslovnik.Positions.each(function(position) {
-            positionHtml += '<option value="'+position.get('id')+'">'+position.get('name')+'</option>';
-        });
-        positionHtml += "</select>";
-        
-        var accountTypeHtml = '<select name="account_type">';
-        accountTypeHtml += '<option value="'+Poslovnik.PermissionLevels.ADMINISTRATOR+'">Administrator</option>';
-        accountTypeHtml += '<option value="'+Poslovnik.PermissionLevels.MODERATOR+'">Moderator</option>';
-        accountTypeHtml += '<option value="'+Poslovnik.PermissionLevels.REGULAR_USER+'">Employee</option>';
-        accountTypeHtml += "</select>";
-        
         var rowHtml = template({
             cid: person.cid,
-            position_html: positionHtml,
-            account_type_html: accountTypeHtml
         });
       
         return rowHtml;
@@ -99,7 +121,7 @@ Poslovnik.AdminDashboard = Backbone.View.extend({
     
     getExistingPersonRowHtml: function(person) {
         var template = "<tr data-cid='<%= cid %>'><td style='text-align: center;'><input name='selected_person' data-id='<%= id %>' type='radio' /></td><td><%= email %></td><td><em>(hidden)</em><td><%= title %></td><td><%= first_name %></td><td><%= last_name %></td><td><%= position %></td><td><%= account_type %></td>";
-           template += "<td><a href='javascript: void(0);' style='font-size: 16px' data-cid='<%= cid %>' class='delete-row glyphicon glyphicon-remove' data-cid='<%= cid %>'></a></td>";
+           template += "<td><a data-cid='<%= cid %>' href='javascript: void(0);' class='edit-row glyphicon glyphicon-pencil'></a>&nbsp;<a href='javascript: void(0);' style='font-size: 16px' data-cid='<%= cid %>' class='delete-row glyphicon glyphicon-remove' data-cid='<%= cid %>'></a></td>";
            template += "</tr>";
             
            var template = _.template(template); 
@@ -206,6 +228,19 @@ Poslovnik.AdminDashboard = Backbone.View.extend({
         );
     },
     
+    onEditRowBtnClick: function(event) {
+        var target = $(event.target);
+        
+        var cid = $(target).attr('data-cid');
+        
+        var model = this.personCollection.get({ cid: cid });
+        
+        var row = this.$el.find('tr[data-cid='+cid+']');
+        
+        var rowHtml = this.getPersonEditRowHtml(model);
+        
+        row.html(rowHtml);
+    },
     
     
     onDeleteRowBtnClick: function(event) {
