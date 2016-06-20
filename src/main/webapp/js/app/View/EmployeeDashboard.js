20110195eb79244746e339b93ce3d6c6032aeb3e
Mihailo Joksimovic <tinzey@gmail.com>
2016-06-21 00:20:00 +0200

    Sitne izmene na Employee dashboardu

--- src/main/webapp/js/app/View/EmployeeDashboard.js
+++ src/main/webapp/js/app/View/EmployeeDashboard.js
@@ -9,11 +9,13 @@ Poslovnik.EmployeeDashboard = Backbone.View.extend({
         var tpl = _.template($('#tpl-employee-dashboard').html());
         
         this.$el.html(tpl);
+        
+        this.subRender();
 
         return this;
     },
     
     subRender: function() {
-        
+        this.$el.find('.user-first-name').html(Poslovnik.Person.get('first_name'));
     }
 });
